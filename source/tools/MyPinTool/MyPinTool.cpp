88a17a42dcf57d2fd66de59f286bdc69c024195f
Dongliang Mu <mudongliangabcd@gmail.com>
2017-03-29 12:54:37 -0400

    add gs base address for instructions in which operands has gs segment

--- source/tools/MyPinTool/MyPinTool.cpp
+++ source/tools/MyPinTool/MyPinTool.cpp
@@ -28,16 +28,13 @@ std::ostream * sysout = &cerr;
 // Command line switches
 /* ===================================================================== */
 KNOB<string> KnobOutputFile(KNOB_MODE_WRITEONCE,  "pintool",
-    "o", "", "specify file name for MyPinTool output");
+		"o", "", "specify file name for MyPinTool output");
 
 KNOB<BOOL>   KnobCount(KNOB_MODE_WRITEONCE,  "pintool",
-    "count", "1", "count instructions, basic blocks and threads in the application");
-
-KNOB<string> KnobXmmFile(KNOB_MODE_WRITEONCE,  "pintool",
-    "xmm", "", "specify file name for logging xmm registers");
+		"count", "1", "count instructions, basic blocks and threads in the application");
 
 KNOB<string> KnobSysFile(KNOB_MODE_WRITEONCE,  "pintool",
-    "sys", "", "specify file name for logging arguments for system call");
+		"sys", "", "specify file name for logging arguments for system call");
 
 /* ===================================================================== */
 // Utilities
@@ -48,12 +45,12 @@ KNOB<string> KnobSysFile(KNOB_MODE_WRITEONCE,  "pintool",
  */
 INT32 Usage()
 {
-    cerr << "This tool prints out the number of dynamically executed " << endl <<
-            "instructions, basic blocks and threads in the application." << endl << endl;
+	cerr << "This tool prints out the number of dynamically executed " << endl <<
+		"instructions, basic blocks and threads in the application." << endl << endl;
 
-    cerr << KNOB_BASE::StringKnobSummary() << endl;
+	cerr << KNOB_BASE::StringKnobSummary() << endl;
 
-    return -1;
+	return -1;
 }
 
 /* ===================================================================== */
@@ -68,8 +65,8 @@ INT32 Usage()
  */
 VOID CountBbl(UINT32 numInstInBbl)
 {
-    bblCount++;
-    insCount += numInstInBbl;
+	bblCount++;
+	insCount += numInstInBbl;
 }
 
 /* ===================================================================== */
@@ -78,22 +75,23 @@ VOID CountBbl(UINT32 numInstInBbl)
 
 string Val2Str(const void* value, unsigned int size)
 {
-    stringstream sstr;
-    sstr << hex;
-    const unsigned char* cval = (const unsigned char*)value;
-    // Traverse cval from end to beginning since the MSB is in the last block of cval.
+	stringstream sstr;
+	sstr << hex;
+	const unsigned char* cval = (const unsigned char*)value;
+	// Traverse cval from end to beginning since the MSB is in the last block of cval.
 
-    while (size)
-    {
-        --size;
-        sstr << setfill('0') <<setw(2) << (unsigned int)cval[size];
-    }
+	while (size)
+	{
+		--size;
+		sstr << setfill('0') <<setw(2) << (unsigned int)cval[size];
+	}
 
-    return string("0x")+sstr.str();
+	return string("0x")+sstr.str();
 }
 
 
-const char * dumpInstructions(INS ins){
+const char * dumpInstructions(INS ins)
+{
 
 	std::stringstream ss; 
 	ss << INS_Disassemble(ins);
@@ -101,7 +99,8 @@ const char * dumpInstructions(INS ins){
 
 }
 
-std::list<REG> * listRegisters(INS ins){
+std::list<REG> * listRegisters(INS ins)
+{
 
 	std::list<REG> * registers = new std::list<REG>;
 
@@ -116,24 +115,30 @@ std::list<REG> * listRegisters(INS ins){
 	return registers; 
 }
 
-std::list<REG> * listMemRegisters(INS ins){
+std::list<REG> * listMemRegisters(INS ins)
+{
 
-        std::list<REG> * registers = new std::list<REG>;
+	std::list<REG> * registers = new std::list<REG>;
 
-        for (UINT i = 0; i < INS_OperandCount(ins); i++){
+	for (UINT i = 0; i < INS_OperandCount(ins); i++){
 
-                if(INS_OperandIsMemory(ins, i)){
-                        registers->push_back(INS_OperandMemoryBaseReg(ins, i));
-                        registers->push_back(INS_OperandMemoryIndexReg(ins, i));
-                }
-        }
+		if(INS_OperandIsMemory(ins, i)){
+			registers->push_back(INS_OperandMemoryBaseReg(ins, i));
+			registers->push_back(INS_OperandMemoryIndexReg(ins, i));
 
-        return registers;
+			if (INS_OperandMemorySegmentReg (ins, i) == REG_SEG_GS) {
+				registers->push_back(REG_SEG_GS_BASE);
+			}
+		}
+
+	}
+
+	return registers;
 }
 
 
-UINT regWidth2Size(REG reg){
-
+UINT regWidth2Size(REG reg)
+{
 	switch(REG_Width(reg)){
 
 		case REGWIDTH_8:
@@ -141,7 +146,7 @@ UINT regWidth2Size(REG reg){
 
 		case REGWIDTH_16:
 			return 2; 
-	
+
 		case REGWIDTH_32:
 			return 4;
 
@@ -162,17 +167,18 @@ UINT regWidth2Size(REG reg){
 
 		case REGWIDTH_FPSTATE:
 			return 16;			
- 
+
 		default: 
 			return 4;
 	}
 }
 
 
-VOID LogInstDetail(THREADID threadID, ADDRINT address, const CONTEXT *ctx, const char* disasm, void * regdata, void *memdata){
-	
+VOID LogInstDetail(THREADID threadID, ADDRINT address, const CONTEXT *ctx, const char* disasm, void * regdata, void *memdata)
+{
+
 	std::list<REG> * registers = (std::list<REG> *)regdata;
-		
+
 	std::list<REG> * memreg = (std::list<REG> *)memdata;
 
 	*out << threadID << "-" << std::hex << address << "-" << disasm;
@@ -185,9 +191,9 @@ VOID LogInstDetail(THREADID threadID, ADDRINT address, const CONTEXT *ctx, const
 		*out<< name.c_str() << ":";
 
 		PIN_REGISTER regval; 
-		
+
 		PIN_GetContextRegval(ctx, reg, reinterpret_cast<UINT8*>(&regval));
-		
+
 		*out << std::hex << Val2Str(&regval, regWidth2Size(reg));
 	}
 
@@ -200,9 +206,9 @@ VOID LogInstDetail(THREADID threadID, ADDRINT address, const CONTEXT *ctx, const
 		*out<< name.c_str() << ":";
 
 		PIN_REGISTER regval; 
-		
+
 		PIN_GetContextRegval(ctx, reg, reinterpret_cast<UINT8*>(&regval));
-		
+
 		*out << std::hex << Val2Str(&regval, regWidth2Size(reg));
 	}
 
@@ -210,7 +216,8 @@ VOID LogInstDetail(THREADID threadID, ADDRINT address, const CONTEXT *ctx, const
 }
 
 
-VOID Trace(INS ins,  VOID *v){
+VOID Trace(INS ins,  VOID *v)
+{
 
 	INS_InsertCall(ins, IPOINT_BEFORE, (AFUNPTR)LogInstDetail, IARG_THREAD_ID, IARG_INST_PTR, IARG_CONTEXT, IARG_PTR, dumpInstructions(ins), IARG_PTR, (void*)listRegisters(ins), IARG_PTR, (void*)listMemRegisters(ins), IARG_END);
 }
@@ -227,7 +234,7 @@ VOID Trace(INS ins,  VOID *v){
  */
 VOID ThreadStart(THREADID threadIndex, CONTEXT *ctxt, INT32 flags, VOID *v)
 {
-    threadCount++;
+	threadCount++;
 }
 
 /*!
@@ -239,12 +246,12 @@ VOID ThreadStart(THREADID threadIndex, CONTEXT *ctxt, INT32 flags, VOID *v)
  */
 VOID Fini(INT32 code, VOID *v)
 {
-    *out <<  "===============================================" << endl;
-    *out <<  "MyPinTool analysis results: " << endl;
-    *out <<  "Number of instructions: " << insCount  << endl;
-    *out <<  "Number of basic blocks: " << bblCount  << endl;
-    *out <<  "Number of threads: " << threadCount  << endl;
-    *out <<  "===============================================" << endl;
+	*out <<  "===============================================" << endl;
+	*out <<  "MyPinTool analysis results: " << endl;
+	*out <<  "Number of instructions: " << insCount  << endl;
+	*out <<  "Number of basic blocks: " << bblCount  << endl;
+	*out <<  "Number of threads: " << threadCount  << endl;
+	*out <<  "===============================================" << endl;
 }
 
 /*!
@@ -256,141 +263,105 @@ VOID Fini(INT32 code, VOID *v)
  */
 
 
-BOOL Intercept(THREADID, INT32 sig, CONTEXT *ctx, BOOL, const EXCEPTION_INFO *, VOID *){
-    std::cerr << "Intercepted signal " << sig << std::endl;
+BOOL Intercept(THREADID, INT32 sig, CONTEXT *ctx, BOOL, const EXCEPTION_INFO *, VOID *)
+{
+	std::cerr << "Intercepted signal " << sig << std::endl;
 
-
-    //std::ostream * xmmout = &cerr;
-
-
-    string fileName = KnobXmmFile.Value();
-
-
-    FILE * pFile;
-
-    pFile = fopen (fileName.c_str(), "wb");
-
-    PIN_REGISTER regval;
-
-
-    PIN_GetContextRegval(ctx, LEVEL_BASE::REG_XMM0, reinterpret_cast<UINT8*>(&regval));
-   fwrite((char *)(&regval), sizeof(char), regWidth2Size(LEVEL_BASE::REG_XMM0), pFile);
-
-    PIN_GetContextRegval(ctx, LEVEL_BASE::REG_XMM1, reinterpret_cast<UINT8*>(&regval));
-   fwrite((char *)(&regval), sizeof(char), regWidth2Size(LEVEL_BASE::REG_XMM1), pFile);
-
-    PIN_GetContextRegval(ctx, LEVEL_BASE::REG_XMM2, reinterpret_cast<UINT8*>(&regval));
-   fwrite((char *)(&regval), sizeof(char), regWidth2Size(LEVEL_BASE::REG_XMM2), pFile);
-
-    PIN_GetContextRegval(ctx, LEVEL_BASE::REG_XMM3, reinterpret_cast<UINT8*>(&regval));
-   fwrite((char *)(&regval), sizeof(char), regWidth2Size(LEVEL_BASE::REG_XMM3), pFile);
-
-    PIN_GetContextRegval(ctx, LEVEL_BASE::REG_XMM4, reinterpret_cast<UINT8*>(&regval));
-   fwrite((char *)(&regval), sizeof(char), regWidth2Size(LEVEL_BASE::REG_XMM4), pFile);
-
-    PIN_GetContextRegval(ctx, LEVEL_BASE::REG_XMM5, reinterpret_cast<UINT8*>(&regval));
-   fwrite((char *)(&regval), sizeof(char), regWidth2Size(LEVEL_BASE::REG_XMM5), pFile);
-
-    PIN_GetContextRegval(ctx, LEVEL_BASE::REG_XMM6, reinterpret_cast<UINT8*>(&regval));
-   fwrite((char *)(&regval), sizeof(char), regWidth2Size(LEVEL_BASE::REG_XMM6), pFile);
-
-    PIN_GetContextRegval(ctx, LEVEL_BASE::REG_XMM7, reinterpret_cast<UINT8*>(&regval));
-   fwrite((char *)(&regval), sizeof(char), regWidth2Size(LEVEL_BASE::REG_XMM7), pFile);
-
-   fclose(pFile);	
-
-//detach. so that segmentation fault will generate a core dump.	
-    PIN_Detach();
-//do not let the application unblock the signal	
-    return FALSE;
+	//detach. so that segmentation fault will generate a core dump.	
+	PIN_Detach();
+	//do not let the application unblock the signal	
+	return FALSE;
 }
 
-INT32 GetSysArgNum(ADDRINT sys_id) {
-    return sys_map[sys_id].argnum;
+INT32 GetSysArgNum(ADDRINT sys_id)
+{
+	return sys_map[sys_id].argnum;
 }
 
-const char *GetSysName(ADDRINT sys_id) {
-    return sys_map[sys_id].sysname;
+const char *GetSysName(ADDRINT sys_id)
+{
+	return sys_map[sys_id].sysname;
 }
 
-VOID SyscallEntry(THREADID tid, CONTEXT *ctx, SYSCALL_STANDARD std, VOID *v) {
+VOID SyscallEntry(THREADID tid, CONTEXT *ctx, SYSCALL_STANDARD std, VOID *v)
+{
 
-    ADDRINT argval;
+	ADDRINT argval;
 
-    ADDRINT num  = PIN_GetSyscallNumber(ctx, std);
+	ADDRINT num  = PIN_GetSyscallNumber(ctx, std);
 
-    UINT32 argnum = GetSysArgNum(num);
+	UINT32 argnum = GetSysArgNum(num);
 
-    *sysout << std::hex << Val2Str(&num, 4);
-    *sysout << "-";
-    *sysout << std::hex << argnum;
+	*sysout << std::hex << Val2Str(&num, 4);
+	*sysout << "-";
+	*sysout << std::hex << argnum;
 
-    for (UINT32 id=0; id<argnum; id++) {
-	argval = PIN_GetSyscallArgument(ctx, std, id);
-        *sysout << "-" << std::hex << Val2Str(&argval, 4);
-    }
-    //*sysout << "-" << GetSysName(num);
+	for (UINT32 id=0; id<argnum; id++) {
+		argval = PIN_GetSyscallArgument(ctx, std, id);
+		*sysout << "-" << std::hex << Val2Str(&argval, 4);
+	}
+	//*sysout << "-" << GetSysName(num);
 }
 
-VOID SyscallExit(THREADID tid, CONTEXT *ctx, SYSCALL_STANDARD std, VOID *v) {
+VOID SyscallExit(THREADID tid, CONTEXT *ctx, SYSCALL_STANDARD std, VOID *v)
+{
 
-    ADDRINT retval =  PIN_GetSyscallReturn(ctx, std);
-    *sysout << "-" << std::hex << Val2Str(&retval, 4) << endl;
+	ADDRINT retval =  PIN_GetSyscallReturn(ctx, std);
+	*sysout << "-" << std::hex << Val2Str(&retval, 4) << endl;
 }
 
 int main(int argc, char *argv[])
 {
-    // Initialize PIN library. Print help message if -h(elp) is specified
-    // in the command line or the command line is invalid 
-    if( PIN_Init(argc,argv) )
-    {
-        return Usage();
-    }
-    
-    string fileName = KnobOutputFile.Value();
+	// Initialize PIN library. Print help message if -h(elp) is specified
+	// in the command line or the command line is invalid 
+	if( PIN_Init(argc,argv) ) {
+		return Usage();
+	}
 
-    if (!fileName.empty()) {
-	out = new std::ofstream(fileName.c_str());
-    }
+	string fileName = KnobOutputFile.Value();
 
-    string sysFile = KnobSysFile.Value();
+	if (!fileName.empty()) {
+		out = new std::ofstream(fileName.c_str());
+	}
 
-    if (!sysFile.empty()) {
-	sysout = new std::ofstream(sysFile.c_str());
-    }
+	string sysFile = KnobSysFile.Value();
 
-    if (KnobCount)
-    {
+	if (!sysFile.empty()) {
+		sysout = new std::ofstream(sysFile.c_str());
+	}
 
-//	PIN_UnblockSignal(11, TRUE);
-	PIN_InterceptSignal(11, Intercept, 0);
+	if (KnobCount) {
 
-        // Register function to be called to instrument traces
-        INS_AddInstrumentFunction(Trace, 0);
+		//	PIN_UnblockSignal(11, TRUE);
+		PIN_InterceptSignal(11, Intercept, 0);
 
-        // Register function to be called for every thread before it starts running
-//        PIN_AddThreadStartFunction(ThreadStart, 0);
+		// Register function to be called to instrument traces
+		INS_AddInstrumentFunction(Trace, 0);
 
-        // Register function to be called when the application exits
-//        PIN_AddFiniFunction(Fini, 0);
-    }
+		// Register function to be called for every thread before it starts running
+		//        PIN_AddThreadStartFunction(ThreadStart, 0);
 
-    /* functions to get called on system calls */
-    PIN_AddSyscallEntryFunction(SyscallEntry, 0);
-    PIN_AddSyscallExitFunction(SyscallExit, 0);
-    
-    cerr <<  "===============================================" << endl;
-    cerr <<  "This application is instrumented by MyPinTool" << endl;
-    if (!KnobOutputFile.Value().empty()) 
-    {
-        cerr << "See file " << KnobOutputFile.Value() << " for analysis results" << endl;
-    }
-    cerr <<  "===============================================" << endl;
+		// Register function to be called when the application exits
+		//        PIN_AddFiniFunction(Fini, 0);
+	}
 
-    // Start the program, never returns
-    PIN_StartProgram();
-    
-    return 0;
+	/* functions to get called on system calls */
+	PIN_AddSyscallEntryFunction(SyscallEntry, 0);
+	PIN_AddSyscallExitFunction(SyscallExit, 0);
+
+	cerr <<  "===============================================" << endl;
+	cerr <<  "This application is instrumented by MyPinTool" << endl;
+
+	if (!KnobOutputFile.Value().empty()) {
+
+		cerr << "See file " << KnobOutputFile.Value() << " for analysis results" << endl;
+	}
+	cerr <<  "===============================================" << endl;
+
+	// Start the program, never returns
+	PIN_StartProgram();
+
+	return 0;
 }
 
 /* ===================================================================== */
